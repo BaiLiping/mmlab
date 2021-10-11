ac8b3f550fe8c6f90a2e859d378603df0d94b7c7
BaiLiping <blp_engineer@outlook.com>
2021-10-11 15:28:21 +0800

    update

--- Experiment/test_inference.sh
+++
@@ -1,4 +0,0 @@
-python3 inference.py --inference_result='/home/zhubinglab/Desktop/NuScenes_Project/inference_result.pkl'\
-                     --inference_network_pretrained_model='/home/zhubinglab/Desktop/mmdetection3d/checkpoint/centerpoint_01voxel_second_secfpn_circlenms_4x8_cyclic_20e_nus_20201001_135205-5db91e00.pth'\
-                     --inference_network_config='/home/zhubinglab/Desktop/mmdetection3d/configs/centerpoint/centerpoint_01voxel_second_secfpn_circlenms_4x8_cyclic_20e_nus.py' \
-                     --root_directory_for_test_data_point_cloud_data_file='/home/zhubinglab/Desktop/NuScenes_Project/data'  
---
+++ Project_5/test_inference.sh
@@ -0,0 +1,4 @@
+python3 inference.py --inference_result='/root/NuScenes_Project/inference_result.pkl'\
+                     --inference_network_pretrained_model='/root/mmlab/mmdetection3d/checkpoint/centerpoint_01voxel_second_secfpn_circlenms_4x8_cyclic_20e_nus_20201001_135205-5db91e00.pth'\
+                     --inference_network_config='/root/mmlab/mmdetection3d/configs/centerpoint/centerpoint_01voxel_second_secfpn_circlenms_4x8_cyclic_20e_nus.py' \
+                     --root_directory_for_test_data_point_cloud_data_file='/home/zhubinglab/Desktop/NuScenes_Project/data'  
